cd74f9140d2407bb22404c32a5e176ccc5b93bca
Rodrigo Venturi <rodrigo.venturi@projuris.com.br>
2019-08-19 11:28:51 -0300

    deploy

--- index.js
+++ index.js
@@ -9,12 +9,32 @@ const app = express();
 const PORT = 3000;
 // Starts server
 
+var fs = require('fs');
+
+const GoogleImages = require('google-images');
+ 
+const client = new GoogleImages('005468022246788069363:bxurpxfnjsu', 'AIzaSyBrR7LkeqFFMVVIsZlj6NXfjbVebW3xxls');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
 
     request('http://apiadvisor.climatempo.com.br/api/v1/weather/locale/4970/current?token=f37a348bdf887b2d1832b7840f17ab0f', { json: true }, function (err, response, body) {
-        res.json(body.data.condition);
+        //res.json(body.data.condition);
+
+        console.log(body.data);
+        var text = body.data.condition + ' Tempo';
+        console.log(text);
+
+        var casaco = 'SIM';
+        if(body.data.sensation > 20) {
+            casaco = 'NÃO';
+        }
+
+        client.search(text)
+        .then(images => {
+            res.json({imagem: images[0].url, 'usar casaco ? ': casaco});
+        });
     })
 });
 
